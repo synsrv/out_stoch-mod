5d107a66cd6d0b20461db4851bc9ec3dc384e499
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2020-03-26 16:01:27 +0100

    add <---T---> arrow [#4]

--- gx_stoch-lts-model_up-bound.py
+++ gx_stoch-lts-model_up-bound.py
@@ -8,13 +8,13 @@ style.use('classic')
 import matplotlib.pyplot as pl
 from matplotlib import rc
 
-rc('text', usetex=True)
+matplotlib.rc('text', usetex=True)
 pl.rcParams['text.latex.preamble'] = [
-    r'\usepackage{tgheros}',   
-    r'\usepackage{sansmath}',  
-    r'\sansmath'               
-    r'\usepackage{siunitx}',   
-    r'\sisetup{detect-all}',   
+    r'\usepackage{tgheros}',    
+    r'\usepackage[eulergreek]{sansmath}',   
+    r'\sansmath',
+    r'\usepackage{siunitx}',    
+    r'\sisetup{detect-all}'
 ]  
 
 import argparse, sys, os, itertools, pickle
@@ -27,8 +27,6 @@ def bm_upbound_figure():
     fig, ax = pl.subplots()
     fig.set_size_inches(6./2,1.5)
 
-    bin_w = 1
-
     dpath = "./data/brownian-motion/" +\
             "200326_145800_brownian-motion_traces-with-up-cap/" +\
             "data/0000"
@@ -39,33 +37,22 @@ def bm_upbound_figure():
     with open(dpath+'/xt.p', 'rb') as pfile:
         xt=np.array(pickle.load(pfile))
 
-    with open(dpath+'/lts.p', 'rb') as pfile:
-        lts=np.array(pickle.load(pfile))
-
 
     idx = 15
     xlow, xhigh = 15, nsp['Nsteps']-1
-
-    
-
-        
     # xlow, xhigh = 100, 850
 
-    lts_select = lts[lts[:, -1]==idx]
 
     sep = np.where(xt[:,idx]==nsp['X_0'])[0]
-
     # for x in sep:
     #     ax.axvline(x)
-    
-        
-    print(lts_select)
 
-    # ax.plot(range(xlow, xhigh), xt[xlow:xhigh,idx])
-
-    ax.plot(range(xlow, sep[0]), xt[xlow:sep[0],idx], color='grey')
-    ax.plot(range(sep[0], sep[1]), xt[sep[0]:sep[1],idx], color='grey')
-    ax.plot(range(sep[1], sep[2]), xt[sep[1]:sep[2],idx], color='grey')
+    ax.plot(range(xlow, sep[0]), xt[xlow:sep[0],idx],
+            color='grey', alpha=0.7)
+    ax.plot(range(sep[0], sep[1]), xt[sep[0]:sep[1],idx],
+            color='grey', alpha=0.7)
+    ax.plot(range(sep[1], sep[2]), xt[sep[1]:sep[2],idx],
+            color='grey', alpha=0.7)
 
     # I'm skipping a few values here purely for visualization purposes
     # exact values are not important here as this grophic only explains
@@ -73,16 +60,17 @@ def bm_upbound_figure():
     ax.plot(range(sep[2]+3, sep[3]), xt[sep[2]+3:sep[3],idx], color='black')
 
     # skipping some values here as well, same reasoning as above
-    ax.plot(range(sep[3]+3, xhigh), xt[sep[3]+3:xhigh,idx], color='grey')
+    ax.plot(range(sep[3]+3, xhigh), xt[sep[3]+3:xhigh,idx],
+            color='grey', alpha=0.7)
     
 
+    # upper boundaries    
     ax.plot([xlow,xhigh],[nsp['up_cap']]*2, color='black')
 
     ax.plot([xlow,xhigh],[nsp['up_cap']*1.4]*2, ':', color='black')
 
-    # ax.plot(xva, yva, 'grey')
+    
 
-    # print(yva)
 
 
 
@@ -107,7 +95,7 @@ def bm_upbound_figure():
     #     else:
     #         l+=1
 
-    # ax.plot(range(k,nsp["Nsteps"]-125),xt[i][l:l+nsp["Nsteps"]-k-125], 'grey')
+    # ax.plot(range(k,nsp["Nsteps"]-125),xt[i][l:l+nsp["Nsteps"]-k-125], 'grey', alpha=0.7)
 
     # ax.plot([k, nsp["Nsteps"]-125*1.05],[0]*2, 'black', linestyle=':')
 
@@ -145,23 +133,25 @@ def bm_upbound_figure():
 
     ax.tick_params(axis='x', which='major', pad=7)
 
-    # l = k-z
-    # mid = z+(k-z)/2
-    # sep = 30
-    # hl=(k-z)*0.025
-    # hw=0.03
+    z,k = sep[2], sep[3]
+    
+    l = k-z
+    mid = z+(k-z)/2
+    sep = 30
+    hl=(k-z)*0.025
+    hw=0.03
 
-    # ax.arrow(mid-sep,-0.35, -(l/2-sep), 0., clip_on=False, shape='full',
-    #          head_width=hw, head_length=hl,
-    #          head_starts_at_zero=False, color='k',
-    #          length_includes_head=True)
+    ax.arrow(mid-sep,-0.35, -(l/2-sep), 0., clip_on=False, shape='full',
+             head_width=hw, head_length=hl,
+             head_starts_at_zero=False, color='k',
+             length_includes_head=True)
 
-    # ax.arrow(mid+sep,-0.35, (l/2-sep), 0., clip_on=False, shape='full',
-    #          head_width=hw, head_length=hl,
-    #          head_starts_at_zero=False, color='k',
-    #          length_includes_head=True)
+    ax.arrow(mid+sep,-0.35, (l/2-sep), 0., clip_on=False, shape='full',
+             head_width=hw, head_length=hl,
+             head_starts_at_zero=False, color='k',
+             length_includes_head=True)
 
-    # ax.text(mid, -0.38, '$T$', fontdict={'ha': 'center'}, clip_on=False)
+    ax.text(mid, -0.38, '$T$', fontdict={'ha': 'center'}, clip_on=False)
 
 
     # ax.arrow(z,-0.35, k-z, 0., clip_on=False, shape='full',
